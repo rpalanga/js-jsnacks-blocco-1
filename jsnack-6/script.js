e6b65b91c1101cb209b28f3eb230469ece32dd9b
roberto-palanga <r.palanga94@gmail.com>
2024-02-02 16:21:47 +0100

    first commit

---
+++ jsnack-6/script.js
@@ -0,0 +1,12 @@
+
+// L’utente inserisce due parole in successione, con due prompt.
+
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+
+let firstWord = prompt("Quanto è quotato?")
+
+let secondtWord = prompt("Siamo sicuri?")
+
+console.log(firstWord)
+console.log(secondWord)
+
